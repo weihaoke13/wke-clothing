0c9b3b6b6f7540a7d23405a5d80fdd6d4d287925
weihaoke13 <weihaoke13@gmail.com>
2021-12-28 15:09:27 -0500

    updating our app with shop page, preview colletion coponent and collection item component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE </h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/preview-collection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item';
+
+import './preview-collection.scss';
+
+const PreviewCollection = ({title, items}) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className='preview'>
+            {
+                items
+                .filter((item,idx) => idx <4)
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps}></CollectionItem>
+                ))
+            }
+        </div>
+    </div>
+)
+
+export default PreviewCollection;
