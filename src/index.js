1debe50c146185c04fe62c1a428d35d072b65a9b
weihaoke13 <weihaoke13@gmail.com>
2022-01-09 17:38:14 -0500

    redux persist + directory state into redux, collection state into redux

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import { Provider } from 'react-redux';
 
 import {BrowserRouter} from 'react-router-dom';
 
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store';
+
 
 ReactDOM.render(
 
   <Provider store ={store} >
     <BrowserRouter>
-        <App />
+        <PersistGate persistor ={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data'
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+// import PreviewCollection from '../../components/preview-collection/preview-collection'
 
-import PreviewCollection from '../../components/preview-collection/preview-collection'
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
+const ShopPage = ({collections})=> (
+   
+        <div className='shop-page' >
+            <CollectionsOverview></CollectionsOverview>
+        </div>
+);
 
-        this.state={
-            collections:SHOP_DATA
-        }
-
-    }
-
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className='shop-page' >
-                {collections.map(({id, ...otherCollectionProps }) => (
-                    <PreviewCollection key={id} {...otherCollectionProps} />
-                ))
-                }
-            </div>
-        )
-    }
-}
 
 export default ShopPage;
